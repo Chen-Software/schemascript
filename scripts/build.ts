90d2c42103f829b9179b36c534dc63f3b9e9d096
Yumin Chen <yumin@chen.so>
2026-02-28 08:26:37 +0000

    Fix build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,6 +6,27 @@ const buildTime = new Date().toISOString();
 const gitCommit = await $`git rev-parse HEAD`.text();
 
 await Bun.build({
+  entrypoints: ["./src/data/schemas/index.ts"],
+  outdir: "./out/schemas",
+  define: {
+    BUILD_VERSION: JSON.stringify(version.trim()),
+    BUILD_TARGET: JSON.stringify(target),
+    BUILD_TIME: JSON.stringify(buildTime),
+    GIT_COMMIT: JSON.stringify(gitCommit.trim()),
+  },
+  target: "node",
+  format: "esm",
+});
+
+const schemaFilePath = "./out/schemas/index.js";
+const generateSql = await $`BUILD_TARGET=SQLite drizzle-kit generate --dialect sqlite --schema ${schemaFilePath}`;
+
+if (generateSql.exitCode !== 0) {
+  console.error("Error generating SQL:", generateSql.stderr);
+  process.exit(1);
+}
+
+await Bun.build({
   entrypoints: ["./src/main.ts"],
   outdir: "./dist",
   define: {
@@ -14,6 +35,8 @@ await Bun.build({
     BUILD_TIME: JSON.stringify(buildTime),
     GIT_COMMIT: JSON.stringify(gitCommit.trim()),
   },
+  target: "node",
+  format: "esm",
 });
 
-console.log(`Built with version ${version.trim()}, commit ${gitCommit.trim()}, build time ${buildTime}.`);
+console.info(`Built with version ${version.trim()}, commit ${gitCommit.trim()}, build time ${buildTime}.`);
