e70e1f2479722fa5fdf5421a2a24352cae50f1eb
Yumin Chen <yumin@chen.so>
2026-03-03 21:27:47 +0000

    Add initial @artefact/schemascript

---
+++ src/artefact/schemascript/core/_constant.ts
@@ -0,0 +1,26 @@
+import { sql } from "@/data/proxies/sqlite";
+
+const SqlValue = () => ({
+	now: sql`CURRENT_TIMESTAMP`,
+	emptyArray: sql`'[]'`,
+});
+
+const sqlValue = SqlValue();
+
+const sqlConstant = () => ({
+	now: sqlValue.now,
+	emptyArray: sqlValue.emptyArray,
+});
+
+const constant = () => ({
+	now: "now",
+	emptyArray: "[]",
+});
+
+// @ts-expect-error
+const BUILD_TARGET = process.env.BUILD_TARGET;
+
+const Constant = () =>
+	(BUILD_TARGET === "SQLite" && sqlConstant()) || constant();
+
+export { Constant };
---
+++ src/artefact/schemascript/index.ts
@@ -0,0 +1,6 @@
+export * from "./core/field";
+export * from "./core/primitive";
+export * from "./core/property";
+export * from "./core/schema";
+export * from "./core/table";
+export * from "./core/value";
